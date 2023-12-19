2d44e1a12d44cf2ec9a1ce713ef714e1f2e3334e
Vitaliy <vitalyazems@gmail.com>
2023-12-19 21:42:21 +0600

    add menu component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: false },
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    'nuxt-icon',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+  ],
+  shadcn: {
+    prefix: 'Ui',
+    componentDir: './components/ui',
+  },
+});
