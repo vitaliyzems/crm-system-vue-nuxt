61a5567545e271ecda7563d823f0b6997bf2f202
Vitaliy <vitalyazems@gmail.com>
2023-12-21 22:34:43 +0600

    done with vue-query and card on index page

---
+++ components/kanban/useKanbanQuery.ts
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/vue-query';
+import { KANBAN_DATA } from './kanban.data';
+import type { ICard } from './kanban.types';
+import type { IDeal } from '~/types/deals.types';
+
+export function useKanbanQuery() {
+  return useQuery({
+    queryKey: ['deals'],
+    queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
+    select(data) {
+      const newBoard = [...KANBAN_DATA];
+      const deals = data.documents as unknown as IDeal[];
+
+      for (const deal of deals) {
+        const column = newBoard.find((col) => col.id === deal.status);
+        if (column) {
+          column.items.push({
+            $createdAt: deal.$createdAt,
+            id: deal.$id,
+            name: deal.name,
+            price: deal.price,
+            companyName: deal.customer.name,
+            status: column.name,
+          });
+        }
+      }
+
+      return newBoard;
+    },
+  });
+}
