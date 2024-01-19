785efdb8e470f0f44d08c4f16f8d42546e5af429
Vitaliy <vitalyazems@gmail.com>
2024-01-19 22:38:38 +0600

    Comments feature done

--- components/kanban/slideover/Comments.vue
+++ components/kanban/slideover/Comments.vue
@@ -1,5 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDealSlideStore } from '#imports';
+import dayjs from 'dayjs';
+import type { IDeal } from '~/types/deals.types';
+import { useComments } from './useComments';
+import { useCreateComment } from './useCreateComment';
+
+const { data, refetch, isLoading } = useComments();
+const { commentRef, writeComment } = useCreateComment({ refetch });
+
+const card = data as unknown as IDeal;
+</script>
 
 <template>
-  <div></div>
+  <UiInput
+    placeholder="Оставьте комментарий"
+    v-model="commentRef"
+    @keyup.enter="writeComment"
+  />
+
+  <UiSkeleton v-if="isLoading" class="w-full h-[76px] rounded mt-5" />
+  <div v-else-if="card">
+    <div
+      v-for="comment in card.comments"
+      :key="comment.$id"
+      class="flex items-start mt-5"
+    >
+      <Icon name="radix-icons:chat-bubble" class="mr-3 mt-1" size="25" />
+
+      <div class="border-border bg-black/20 rounded p-3 w-full">
+        <div class="mb-2 text-sm">
+          Комментарий {{ dayjs(comment.$createdAt).format('HH:mm') }}
+        </div>
+        <p>{{ comment.text }}</p>
+      </div>
+    </div>
+  </div>
 </template>
---
+++ components/kanban/slideover/useCreateComment.ts
@@ -0,0 +1,32 @@
+import { useDealSlideStore } from '#imports';
+import { useMutation } from '@tanstack/vue-query';
+import { v4 as uuid } from 'uuid';
+import { DB_ID, COLLECTION_COMMENTS } from '#imports';
+
+export function useCreateComment({ refetch }: { refetch: () => void }) {
+  const store = useDealSlideStore();
+  const commentRef = ref<string>();
+
+  const { mutate } = useMutation({
+    mutationKey: ['add comments', commentRef.value],
+    mutationFn: () =>
+      DB.createDocument(DB_ID, COLLECTION_COMMENTS, uuid(), {
+        text: commentRef.value,
+        deal: store.card?.id,
+      }),
+    onSuccess: () => {
+      refetch();
+      commentRef.value = '';
+    },
+  });
+
+  const writeComment = () => {
+    if (!commentRef.value) return;
+    mutate();
+  };
+
+  return {
+    writeComment,
+    commentRef,
+  };
+}
